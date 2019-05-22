008e50eeee97ee1b534278f07e30b6d3a8def283
Mikael Svenson <miksvenson@gmail.com>
2019-05-22 16:48:56 -0700

    Cleanup

--- Enable-Naming-Policy-AD.ps1
+++ Enable-Naming-Policy-AD.ps1
@@ -1,31 +1,29 @@
-Connect-AzureAD
+$tenantid = (Get-Content .\tenantid.txt).Trim()
+$appid = (Get-Content .\appid.txt).Trim()
+$thumb = (Get-Content .\cert-thumb.txt).Trim()
+$connection = Connect-AzureAD -TenantId $tenantid -ApplicationId $appid -CertificateThumbprint $thumb
+
 $group = Get-AzureADGroup -SearchString "O365-Admin"
 # Fetch existing settings if set
 $policySetting = Get-AzureADDirectorySetting | Where-Object {$_.DisplayName -eq "Group.Unified"}
 if ($null -eq $policySetting) {
    # Retrieve the Group.Unified settings template (assuming you have not done this before)
-   $template = Get-AzureADDirectorySettingTemplate | Where-Object {$_.DisplayName -eq "Group.Unified"}
-   
+   $template = Get-AzureADDirectorySettingTemplate | Where-Object {$_.DisplayName -eq "Group.Unified"}   
    # Create the settings object from the template
    $settings = $template.CreateDirectorySetting()
-   
-   # Use this settings object to prevent others than specified group to create Groups
-   $settings["EnableGroupCreation"] = $false
-   $settings["GroupCreationAllowedGroupId"] = $group.ObjectId
-   
-   # (optional) Add a link to the Group usage guidelines
-   $settings["UsageGuidelinesUrl"] = "https://<tenant>.sharepoint.com/SitePages/Guidelines.aspx"
    $policySetting = New-AzureADDirectorySetting -DirectorySetting $settings
 }
-else {
-   $policySetting["EnableGroupCreation"] = $false
-   $policySetting["GroupCreationAllowedGroupId"] = $group.ObjectId
-   
-   # (optional) Add a link to the Group usage guidelines
-   $policySetting["UsageGuidelinesUrl"] = "https://<tenant>.sharepoint.com/SitePages/Guidelines.aspx"
 
-   Set-AzureADDirectorySetting -Id $policySetting.Id -DirectorySetting $policySetting
-}
+
+# (optional) Block group creation - only admins
+#$policySetting["EnableGroupCreation"] = $false
+
+# (optional) Allow a certain group to create
+#$policySetting["GroupCreationAllowedGroupId"] = $group.ObjectId
+
+# (optional) Add a link to the Group usage guidelines
+#$policySetting["UsageGuidelinesUrl"] = "https://<tenant>.sharepoint.com/SitePages/Guidelines.aspx"
 
 $policySetting["PrefixSuffixNamingRequirement"] = "Grp_[Department]_[GroupName]"
+$policySetting["CustomBlockedWordsList"] = "poker"
 Set-AzureADDirectorySetting -Id $policySetting.Id -DirectorySetting $policySetting
